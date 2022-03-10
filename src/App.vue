6dcb1997d1511ae3e582f051c5fbc4a20740fa83
sanya buryat <alexandrbnimaev@yandex.ru>
2022-03-10 21:16:12 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
+  background-color: $bg-color;
 }
 </style>
